5d3dbc42a7a2934a2bdfd7b44fb1ddfb50cb1b3e
302_Pochtov_AA <100467841+kotkrest@users.noreply.github.com>
2022-05-21 14:17:28 +0400

    Add files via upload

---
+++ Task04/src/StackInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PochtovAA\Task04;
+
+interface StackInterface
+{
+    public function push(...$Array);
+    public function pop();
+    public function top();
+    public function isEmpty();
+    public function copy();
+    public function __toString();
+}
---
+++ Task04/src/run_test.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PochtovAA\Task04;
+
+require __DIR__ . '/../vendor/autoload.php';
+use PochtovAA\Task04\Stack;
+use PochtovAA\Task04\StackInterface;
+
+runTest();
